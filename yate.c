093f86fd599339cf7483b55c370a61e782abd8ab
Ben Iofel <iofelben@gmail.com>
2014-04-15 23:10:41 -0400

    made smaller

--- yate.c
+++ yate.c
@@ -1,29 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
-
-FILE *fp;
-
-void end(int signal){
-	fclose(fp);
-	exit(0);
-}
-
-int main(int argc, char *argv[]){
-	if (argc != 2){
-		fprintf(stderr, "Usage: %s <file name>\n", argv[0]);
-		return 1;
-	}
-
-	if ((fp = fopen(argv[1], "a")) == NULL){
-		fprintf(stderr, "Error: can't open %s for writing\n", argv[1]);
-		return 1;
-	}
-	
-	signal(SIGINT, end);	
-	printf("ANYTHING YOU TYPE WILL BE WRITTEN TO THE FILE\n^C TO SAVE AND EXIT\n\n");
-
-	while(1)
+main(int i, char**v){
+	void *fp = fopen(v[1], "a");
+	for(;;){
 		fprintf(fp, "%c", getchar());
+		fflush(fp);
+	}
 }
-
