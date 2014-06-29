72c8cd0b77b4210f2e494345a626936bb732b00e
infoburp <gwolfendale@gmail.com>
2014-06-29 07:35:16 +0100

    smaller

--- yate.c
+++ yate.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 main(int i, char**v){
 	void *fp = fopen(v[1], "a+");
-	int c;
-	if (fp) {
-    		while ((c = getc(fp)) != EOF)
-        		putchar(c);
-	}	
-for(;;){
+	int c=0;
+	while ((c = getc(fp)) != EOF)
+        	putchar(c);	
+	for(;;){
 		fprintf(fp, "%c", getchar());
 		fflush(fp);
 	}
