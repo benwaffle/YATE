f3eea87ab91bda842c106666a0cdf9716e792427
benwaffle <benwaffle@users.noreply.github.com>
2014-11-16 23:13:23 -0500

    its not 1970
    we dont need 80 char line limit
    also v[1] because makes sense

--- yate.c
+++ yate.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 main(int c, char **v){
-	FILE *f = fopen(*++v, "a");
-	for(setvbuf(f, NULL, _IONBF, 0); (c=getchar()) != EOF;
-	    fprintf(f, "%c", c));
+	FILE *f = fopen(v[1], "a");
+	for(setvbuf(f, NULL, _IONBF, 0); (c=getchar()) != EOF; fprintf(f, "%c", c));
 }
